cb0e016d06ad49dfeee72314fb04578fca64629a
Donald Whitely <dswhitely1@gmail.com>
2020-01-02 11:27:32 -0500

    Added Lint Script

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,9 @@ module.exports = {
   production: {
     client: 'pg', // Install this package
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2, max: 10
+    },
     migrations: {
       directory: './database/migrations',
     },
